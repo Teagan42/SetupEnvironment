93196c955359c9fe3faf905ec4ad074d6ff1ce5b
Teagan Glenn <teagan@Zaphod.hsd1.co.comcast.net>
2016-04-13 20:58:40 -0600

    Retrieve dot file repo and link to home folder

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
-sudo apt-get -y --force-yes zsh
-sudo chmod 0777 *.sh
+GREEN='\033[0;31m'
+NC='\033[0m'
 
-./ffmpeg.install.sh
+printf "${GREEN}Installing packages${NC}"
+sudo apt-get -y --force-yes zsh python3-pip git
+
+printf "${GREEN}Installing Google Chrome\n${NC}"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+printf "${GREEN}Running Installation Scripts\n${NC}"
+#sudo chmod 0777 *.sh
+
+#./ffmpeg.install.sh
+
+printf "${GREEN}Dot files\n${NC}"
+mkdir ~/src/Teagan42/
+cd ~/src/Teagan42/
+git clone https://github.com/Teagan42/DotFiles.git
+cd DotFiles
+cp -r .zsh/ ~
+cp -r .vim/ ~
+cp .zshrc ~
+cp .vimrc ~
