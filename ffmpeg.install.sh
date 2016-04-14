c70b54ff8d52eae8026164ed90605f077eda065a
Teagan Glenn <teagan@Zaphod.hsd1.co.comcast.net>
2016-04-13 22:00:52 -0600

    Use repos if possible

--- ffmpeg.install.sh
+++ ffmpeg.install.sh
@@ -1,83 +1,54 @@
-# Install Dependencies
-sudo apt-get update
-sudo apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev \
-  libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
-  libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
-  
+RCol='\e[0m'    # Text Reset
+GREEN='\e[0;32m'
+RED='\e[0;31m'
+
+sudo apt-get -y --force-yes install yasm libmp3lame-dev libopus-dev libx264-dev cmake mercurial
+
 mkdir ~/ffmpeg_sources
 
-# YASM
-sudo apt-get -y --force-yes install yasm
-
-# H.264 Video Encoder
+printf "${GREEN}Installing libfdk-aac\n${RCol}"
 cd ~/ffmpeg_sources
-wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2
-tar xjvf last_x264.tar.bz2
-cd x264-snapshot*
-PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
-PATH="$HOME/bin:$PATH" make
+printf "Retrieving package\n"
+wget -O fdk-aac.tar.gz https://github.com/mstorsjo/fdk-aac/tarball/master
+tar xzvf fdk-aac.tar.gz
+cd mstorsjo-fdk-aac*
+autoreconf -fiv
+printf "Building package\n"
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
 make install
 make distclean
 
-# H.265 Video Encoder
-sudo apt-get -y --force-yes install cmake mercurial
+printf "${GREEN}Installing libx265\n${RCol}"
 cd ~/ffmpeg_sources
+printf "Retrieving package\n"
 hg clone https://bitbucket.org/multicoreware/x265
-cd x265/build/linux
+cd ~/ffmpeg_sources/x265/build/linux
+printf "Building package\n"
 PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
 make
 make install
 make distclean
 
-# AAC Audio Encoder
-sudo apt-get -y --force-yes install libtool
+printf "${Green}Installing libvpx\n${RCol}"
 cd ~/ffmpeg_sources
-wget -O fdk-aac.tar.gz https://github.com/mstorsjo/fdk-aac/tarball/master
-tar xzvf fdk-aac.tar.gz
-cd mstorsjo-fdk-aac*
-autoreconf -fiv
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
-make
-make install
-make distclean
-
-# MP3 Audio Encoder
-sudo apt-get -y --force-yes install nasm
-cd ~/ffmpeg_sources
-wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-tar xzvf lame-3.99.5.tar.gz
-cd lame-3.99.5
-./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --disable-shared
-make
-make install
-make distclean
-
-# Opus Audio Decoder/Encoder
-cd ~/ffmpeg_sources
-wget http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz
-tar xzvf opus-1.1.tar.gz
-cd opus-1.1
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
-make
-make install
-make clean
-
-# VPX Video Decoder/Encoder
-cd ~/ffmpeg_sources
+printf "Retrieving package\n"
 wget http://storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.5.0.tar.bz2
 tar xjvf libvpx-1.5.0.tar.bz2
 cd libvpx-1.5.0
+printf "Installing package\n"
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests
 PATH="$HOME/bin:$PATH" make
 make install
 make clean
 
-# Compile FFMPEG
-cd ~/ffmpeg_sources
+printf "${GREEN}Installing ffmpeg\n${RCol}"
+printf "Retrieving package\n"
 wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
-PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+printf "Installing package\n"
+printf "${GREEN}TH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
   --prefix="$HOME/ffmpeg_build" \
   --pkg-config-flags="--static" \
   --extra-cflags="-I$HOME/ffmpeg_build/include" \
@@ -100,6 +71,3 @@ make install
 make distclean
 hash -r
 
-# Cleanup
-cd ~
-sudo rm -rf ~/ffmpeg_sources
